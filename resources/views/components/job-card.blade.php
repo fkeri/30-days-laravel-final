fc775d57317072f4db3c5da66733e36bf16a0503
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-16 15:56:15 +0100

    Day 27: add tag component

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -8,12 +8,9 @@
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <a href="#"
-                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
-            <a href="#"
-                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
-            <a href="#"
-                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
         </div>
 
         <img src="http://placehold.it/42/42" alt="Job Image" class="rounded-xl">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,19 @@
 
         <section>
             <x-section-heading>Tags</x-section-heading>
+
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
         </section>
 
         <section>
