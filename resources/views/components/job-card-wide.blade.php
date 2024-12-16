ee3d2c2f2a1a31a7a93be38d1a8a55e71db28da8
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-16 16:34:39 +0100

    Day 28: add job-card-wide component

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+    <div>
+        <img src="http://picsum.photos/seed/{{ rand(0, 1000) }}/100/100" alt="Job Image" class="rounded-xl">
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div class="">
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
